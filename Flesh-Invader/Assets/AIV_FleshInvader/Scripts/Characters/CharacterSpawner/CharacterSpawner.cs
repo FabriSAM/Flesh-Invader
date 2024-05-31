a9470f147cfe251747b29c55a593de0761029d41
Marco-Pungillo <pungillomarco@gmail.com>
2024-05-31 20:14:07 +0200

    Spawn Area Pooling
    ADD:
    • Pooler
    • Fixed RogueCharacter prefab

--- Flesh-Invader/Assets/AIV_FleshInvader/Scripts/Characters/CharacterSpawner/CharacterSpawner.cs
+++ Flesh-Invader/Assets/AIV_FleshInvader/Scripts/Characters/CharacterSpawner/CharacterSpawner.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterSpawner : MonoBehaviour
+public class CharacterSpawner : MonoBehaviour, IPoolRequester
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private PoolData[] characterType;
+    [SerializeField] protected float spawnRadius;
+    [SerializeField] protected float spawnTime;
+    [SerializeField] protected float spawnTimeCounter;
+
+
+    public PoolData[] Datas
     {
-        
+        get { return characterType; }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        spawnTimeCounter += Time.deltaTime;
+
+        if (spawnTimeCounter >= spawnTime)
+        {
+            spawnTimeCounter = 0;
+            SpawnCharacter();
+        }
     }
+
+    private void SpawnCharacter()
+    {
+        Character characterToSpawn = Pooler.Instance.GetPooledObject(characterType[Random.Range(0, characterType.Length)]).GetComponent<Character>();
+        Vector3 spawnOffset = Random.insideUnitSphere * spawnRadius; 
+        spawnOffset.y = 0;
+        characterToSpawn.transform.position = PlayerState.Get().transform.position + spawnOffset;
+        characterToSpawn.gameObject.SetActive(true);
+    }
+
+
 }
