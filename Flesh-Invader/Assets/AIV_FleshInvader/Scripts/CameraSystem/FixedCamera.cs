9af8855a95af29c3304dfe2a89839616398d7a47
alex28931 <alex28931@gmail.com>
2024-06-12 18:50:37 +0200

    CameraForPossessionEnded
    Manca la chiamata ai GlobalEvent nel PossessionBullet

--- Flesh-Invader/Assets/AIV_FleshInvader/Scripts/CameraSystem/FixedCamera.cs
+++ Flesh-Invader/Assets/AIV_FleshInvader/Scripts/CameraSystem/FixedCamera.cs
@@ -1,25 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class FixedCamera : MonoBehaviour
 {
+    #region Consts
+    private const float minFov = 10.0f;
+    private const float maxFov = 120.0f;
+    private const float defaultFovSpeed = 50.0f;
+    #endregion
+
+    #region SerializeField
     [SerializeField]
     private float cameraSpeed;
+    [SerializeField]
+    private Camera ownerCamera;
+    #endregion
 
     #region Private Members
     private Vector3 offset;
     private Vector3 newPos;
     private Vector3 currentPos;
+    private float defaultFov;
     private Coroutine cameraShakeCoroutine;
+    private Coroutine cameraFovCoroutine;
     #endregion
-    // Start is called before the first frame update
+
+    #region Mono
     void Start()
     {
         offset = gameObject.transform.position - PlayerState.Get().CurrentPlayer.transform.position;
         newPos = gameObject.transform.position;
         currentPos = gameObject.transform.position;
+        defaultFov = ownerCamera.fieldOfView;
     }
 
     // Update is called once per frame
@@ -27,6 +42,9 @@ public class FixedCamera : MonoBehaviour
     {
         CameraMovement();
     }
+    #endregion
+
+    #region privateMethods
     private void LerpCameraMovement()
     {
         gameObject.transform.position = Vector3.Lerp(currentPos, newPos, cameraSpeed * Time.deltaTime);
@@ -37,11 +55,34 @@ public class FixedCamera : MonoBehaviour
         currentPos = gameObject.transform.position;
         LerpCameraMovement();
     }
-    public void CameraShake(float amplitude, float duration)
+    private void CameraShake(float amplitude, float duration,bool overrideCoroutine)
     {
+        if (overrideCoroutine && cameraShakeCoroutine != null)
+        {
+            StopCoroutine(cameraShakeCoroutine);
+            cameraShakeCoroutine = null;
+        }
+        if (cameraShakeCoroutine != null) return;
         cameraShakeCoroutine = StartCoroutine(CameraShakeCoroutine(amplitude,duration));
     }
+    private void CameraFov(float speed, float endFov, bool overrideCoroutine)
+    {
+        if (overrideCoroutine && cameraFovCoroutine != null) 
+        {
+            StopCoroutine(cameraFovCoroutine);
+            cameraFovCoroutine = null;
+        }
+        if(cameraFovCoroutine != null) return;
+        endFov = Mathf.Clamp(endFov, minFov, maxFov);
+        cameraFovCoroutine=StartCoroutine(CameraFovCoroutine(speed,endFov));
+    }
+    private void ReturnToDefaultFov()
+    {
+        CameraFov(defaultFovSpeed, defaultFov, true);
+    }
+    #endregion
 
+    #region Coroutine
     IEnumerator CameraShakeCoroutine(float amplitude, float duration)
     {
         float currentTime = 0;
@@ -51,6 +92,25 @@ public class FixedCamera : MonoBehaviour
             gameObject.transform.position+=Random.insideUnitSphere*amplitude*Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
-        yield return null;
+        cameraShakeCoroutine = null;
+        yield return new WaitForEndOfFrame();
     }
+
+    IEnumerator CameraFovCoroutine(float speed, float endFov)
+    {
+        if (ownerCamera.fieldOfView == endFov) yield break;
+
+        if (ownerCamera.fieldOfView > endFov)
+        {
+            speed *= -1;
+        }
+        while (ownerCamera.fieldOfView - endFov <= 0.1)
+        {
+            ownerCamera.fieldOfView += speed * Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        cameraFovCoroutine = null;
+        yield return new WaitForEndOfFrame();
+    }
+    #endregion
 }
