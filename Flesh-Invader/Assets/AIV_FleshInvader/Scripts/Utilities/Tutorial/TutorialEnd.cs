f7e269293b1cb3cf875a7aa0431214df5880df99
FabriSAM <fabri.conni@gmail.com>
2024-06-26 00:35:09 +0200

    Tutoral Level

---
+++ Flesh-Invader/Assets/AIV_FleshInvader/Scripts/Utilities/Tutorial/TutorialEnd.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialEnd : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> ActorsInScene;
+    List<GameObject> ActorDeath = new List<GameObject>();
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var actor in ActorsInScene)
+        {
+            if(actor.activeSelf) continue;
+            if (ActorDeath.Contains(actor)) continue;  
+            ActorDeath.Add(actor);
+        }
+        if (ActorsInScene.Count - ActorDeath.Count == 1)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
